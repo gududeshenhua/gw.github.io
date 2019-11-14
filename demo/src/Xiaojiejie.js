90b7fa292e006901a0bc4516e83857dcbb8a69c3
gududeshenhua <1013757892@qq.com>
2019-11-14 18:06:42 +0800

    戏班啦

--- demo/src/Xiaojiejie.js
+++ demo/src/Xiaojiejie.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import XiaojiejieItem from './XiaojiejieItem';  
 class Xiaojiejie extends Component{  
+	//在某一时刻 可以自动执行的函数
 	constructor(props) {
 	    super(props)
 		this.state={
@@ -8,12 +9,33 @@ class Xiaojiejie extends Component{
 			list:['基础按摩','精油推背']
 		} 
 	}
+	componentWillMount(){
+		console.log('1componentWillMount---组件将要挂到页面的时刻')
+	}
+	componentDidMount(){
+		console.log('2componentDidMount---组件完成到页面的时刻')
+	}
+	shouldComponentUpdate(){
+		console.log('3shouldComponentUpdate');
+		return true;  
+	}
+	componentDidUpdate(){
+		console.log('F') 
+	}
+	
 	render(){
-		return (  
+		console.log('4render---组件完成到页面的时刻')
+		return (   
 			<div>
 			   {/* 正确注释的写法 */}
-               <div><input value={this.state.inputValue} onChange={this.inputChange.bind(this)}/> <button onClick={this.addList.bind(this)}> 增加服务 </button></div>
-               <ul> 
+               <div>
+			   <input 
+			    value={this.state.inputValue} 
+			    onChange={this.inputChange.bind(this)}
+				ref={(input)=>{this.input=input}}
+			   />  
+			   <button onClick={this.addList.bind(this)}> 增加服务 </button></div>
+               <ul ref={(ul)=>{this.ul = ul}}> 
                    {
 					   this.state.list.map((item,index)=>{
 						   return ( 
@@ -22,8 +44,9 @@ class Xiaojiejie extends Component{
 								content={item}
 								index={index}
 								deleteItem={this.deleteItem.bind(this)}
+								avname="111" 
 								/>
-						   )//不加（）的话就不可以换行
+						   )//不加（）的话就不可以换行 
 					   })
 				   } 
                </ul>   
@@ -35,14 +58,16 @@ class Xiaojiejie extends Component{
 		console.log(this); 
 		console.log(e.target.value); */
 		this.setState({
-			inputValue:e.target.value 
+			inputValue:this.input.value 
 		})
 	} 
 	addList(){
 		this.setState({
 			list:[...this.state.list,this.state.inputValue]
-		})
-	}
+		},()=>{
+			console.log(this.ul.querySelectorAll('li').length); 
+		});//这是一个异步的方式 回调函数想当于vue $nextTick
+	}  
 	deleteItem(index){
 		let list = this.state.list;
 		list.splice(index,1); 
--- demo/src/XiaojiejieItem.js
+++ demo/src/XiaojiejieItem.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react'; //imrc
+import PropTypes from 'prop-types';
 class XiaojiejieItem extends Component { //cc
    constructor(props) {
        super(props);
 	   this.handleClick = this.handleClick.bind(this);
    }
+   componentWillReceiveProps(){
+	   
+   }
     render() { 
         return ( 
-            <div onClick={this.handleClick}>{this.props.content}</div>
+            <li onClick={this.handleClick}>
+			 {this.props.avname}为你服务-{this.props.content}
+			</li> 
          ); 
     }
 	handleClick(){ 
 		debugger; 
 		this.props.deleteItem(this.props.index);  
 	}  
-}
+}   
+XiaojiejieItem.propTypes = { 
+	avname:PropTypes.string.isRequired, 
+	content:PropTypes.string,
+	index:PropTypes.number,
+	deleteItem:PropTypes.func
+ }  
+ XiaojiejieItem.defaultProps={
+	 avname:'2222' 
+ }
 export default XiaojiejieItem; 
