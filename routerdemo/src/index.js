a187e6b0a946be248f52a0d69c796c95a3d84601
gududeshenhua <1013757892@qq.com>
2019-11-25 14:39:26 +0800

    更新代码

--- routerdemo/src/index.js
+++ routerdemo/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import App from './App';
+import AppRouter from './AppRouter'
 import './index.css';
 
+
 ReactDOM.render(
-  <App />,
+  <AppRouter />, 
   document.getElementById('root')
 );
